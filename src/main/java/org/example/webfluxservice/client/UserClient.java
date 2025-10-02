02cd05492fc3c45cf06dcad263058806a246930f
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-02 23:50:44 +0400

    client service webflux

--- src/main/java/org/example/webfluxservice/client/UserClient.java
+++ src/main/java/org/example/webfluxservice/client/UserClient.java
@@ -1,10 +1,13 @@
 package org.example.webfluxservice.client;
 
 import lombok.Data;
+import org.example.webfluxservice.model.Aircraft;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
-
+@Component
 @Data
 public class UserClient {
 
@@ -16,7 +19,8 @@ public class UserClient {
 
     public List getAllAircraft() {
         String url = "http://localhost:8080/aircraft";
-        return restTemplate.getForObject(url, List.class);
+        Aircraft[] response = restTemplate.getForObject(url, Aircraft[].class);
+        return Arrays.asList(response);
     }
 
 }
--- src/main/java/org/example/webfluxservice/service/UserServiceImpl.java
+++ src/main/java/org/example/webfluxservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ package org.example.webfluxservice.service;
 import lombok.RequiredArgsConstructor;
 import org.example.webfluxservice.client.UserClient;
 import org.example.webfluxservice.model.Aircraft;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
 
     private final UserClient userClient;
 
